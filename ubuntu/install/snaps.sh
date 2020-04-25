ffa4df5359083914a60bc02f32e601e1c55433f9
Clément Tessier <clement.tessier@capgemini.com>
2020-04-25 19:29:36 +0200

    ubuntu: add postman and vlc

--- ubuntu/install/snaps.sh
+++ ubuntu/install/snaps.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
-  && . "../utils.sh"
+    && . "../utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
 
-  print_in_purple "\n    Snaps\n\n"
+    print_in_purple "\n    Snaps\n\n"
 
-  install_snap_package "Spotify" "spotify"
-  install_snap_package "Visual Studio Code" "code"
-  install_snap_package "1Password" "1password-linux"
-  install_snap_package "Microsoft Teams" "teams-for-linux"
-
+    install_snap_package "Spotify" "spotify"
+    install_snap_package "Visual Studio Code" "code"
+    install_snap_package "1Password" "1password-linux"
+    install_snap_package "Postman" "postman"
+    install_snap_package "VLC" "vlc"
 }
 
 main
